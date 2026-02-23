03202ff5f0eca8984b8f9d971c7084790fd4d733
Putrasedana <putrasedana03@gmail.com>
2026-02-23 21:41:39 +0800

    Load real events

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
-import { events } from "@/lib/constants";
 import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
@@ -10,8 +9,8 @@ const Page = async () => {
   "use cache";
   cacheLife("hours");
 
-  // const response = await fetch(`${BASE_URL}/api/events`);
-  // const { events } = await response.json();
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
 
   return (
     <section>
