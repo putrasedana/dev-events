5462722bc8d8031474e083348317dbd7b49277d0
Putrasedana <putrasedana03@gmail.com>
2026-02-23 20:25:23 +0800

    Implement api routes, server actions and caching, and the event details page

---
+++ app/api/events/route.ts
@@ -0,0 +1,99 @@
+import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
+import { Event } from "@/database";
+import connectDB from "@/lib/mongodb";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const formData = await req.formData();
+
+    let event;
+
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (error) {
+      return NextResponse.json(
+        {
+          message: "Invalid JSON data format",
+        },
+        { status: 400 },
+      );
+    }
+
+    const file = formData.get("image") as File;
+
+    if (!file) {
+      return NextResponse.json(
+        {
+          message: "Image file is required",
+        },
+        { status: 400 },
+      );
+    }
+
+    let tags = JSON.parse(formData.get("tags") as string);
+    let agenda = JSON.parse(formData.get("agenda") as string);
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          {
+            resource_type: "image",
+            folder: "DevEvent",
+          },
+          (error, results) => {
+            if (error) return reject(error);
+            resolve(results);
+          },
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
+
+    const createdEvent = await Event.create({ ...event, tags, agenda });
+
+    return NextResponse.json({ message: "Event Created Successfully", event: createdEvent }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating event:", error);
+    return NextResponse.json(
+      {
+        message: "Event Creation Failed",
+        error: error instanceof Error ? error.message : "Unknown error",
+      },
+      { status: 500 },
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const events = await Event.find().sort({ createdAt: -1 });
+
+    return NextResponse.json(
+      {
+        message: "Event fetched successfully",
+        events,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    NextResponse.json(
+      {
+        message: "Event fetching failed",
+        error: error,
+      },
+      { status: 500 },
+    );
+  }
+}
+
+// a route that accepts slug as input -> return the event details
---
+++ lib/actions/action.event.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { Event } from "@/database";
+import connectDB from "../mongodb";
+
+export const getSimilarEventsBySlug = async (slug: string) => {
+  try {
+    await connectDB();
+
+    const event = await Event.findOne({ slug }).lean();
+    if (!event) return [];
+
+    const similarEvents = await Event.find({
+      _id: { $ne: event._id },
+      tags: { $in: event.tags },
+    }).lean();
+
+    return similarEvents.map((item) => ({
+      ...item,
+      _id: item._id.toString(),
+    }));
+  } catch {
+    return [];
+  }
+};
